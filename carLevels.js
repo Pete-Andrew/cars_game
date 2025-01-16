2477565d5f09a26bfc7d200cf14387d0dd8b5446
Peter Andrew <peter.a.andrew@gmail.com>
2025-01-16 08:11:07 +0000

    added additional levels

--- carLevels.js
+++ carLevels.js
@@ -1,21 +1,31 @@
 const levelData = {
 
   0: [
-    { mainCar: true,"x": 450, "y": 300, "width": 300, "height": 150, "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450 } },
-    { "x": 750, "y": 150, "width": 150, "height": 450, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 600, "color": "#ff00f2", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 600 } },
-
+    { "x": 300, "y": 0, "width": 150, "height": 450, "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 450, "color": "#ffff00", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 450 } },
+    { "x": 600, "y": 0, "width": 300, "height": 150, "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150, "color": "#00c500", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150 } },
+    { "x": 0, "y": 300, "width": 300, "height": 150, "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450 } },
+    { "x": 0, "y": 450, "width": 450, "height": 150, "carLeftEdge": 0, "carRightEdge": 450, "carTop": 450, "carBottom": 600, "color": "#ff00f2", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 450, "carTop": 450, "carBottom": 600 } },
   ],
 
   1: [
     { "x": 300, "y": 0, "width": 150, "height": 450, "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 450, "color": "#ffff00", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 450 } },
+    { "x": 600, "y": 0, "width": 300, "height": 150, "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150, "color": "#00c500", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150 } },
+    { "x": 0, "y": 300, "width": 300, "height": 150, "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450 } },
+    { "x": 0, "y": 450, "width": 450, "height": 150, "carLeftEdge": 0, "carRightEdge": 450, "carTop": 450, "carBottom": 600, "color": "#ff00f2", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 450, "carTop": 450, "carBottom": 600 } },
+    { "x": 750, "y": 450, "width": 150, "height": 450, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 450, "carBottom": 900, "color": "#0000ff", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 450, "carBottom": 900 } }
+  ],
+
+
+  2: [
+    { "x": 300, "y": 0, "width": 150, "height": 450, "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 450, "color": "#ffff00", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 450 } },
     { "x": 750, "y": 150, "width": 150, "height": 450, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 600, "color": "#ff00f2", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 600 } },
-    { mainCar: true,"x": 450, "y": 300, "width": 300, "height": 150, "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450 } },
+    { mainCar: true, "x": 450, "y": 300, "width": 300, "height": 150, "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450 } },
     { "x": 0, "y": 450, "width": 450, "height": 150, "carLeftEdge": 0, "carRightEdge": 450, "carTop": 450, "carBottom": 600, "color": "#87cefa", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 450, "carTop": 450, "carBottom": 600 } },
     { "x": 450, "y": 450, "width": 150, "height": 300, "carLeftEdge": 450, "carRightEdge": 600, "carTop": 450, "carBottom": 750, "color": "#00c500", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 600, "carTop": 450, "carBottom": 750 } },
     { "x": 600, "y": 600, "width": 300, "height": 150, "carLeftEdge": 600, "carRightEdge": 900, "carTop": 600, "carBottom": 750, "color": "#ffa500", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 900, "carTop": 600, "carBottom": 750 } }
   ],
 
-  2: [
+  3: [
     { x: 0, y: 0, width: 450, height: 150, carLeftEdge: 0, carRightEdge: 450, carTop: 0, carBottom: 150, color: 'green', orientation: 'hrz', hasMoved: false, initialPosition: { carLeftEdge: 0, carRightEdge: 450, carTop: 0, carBottom: 150 } },
     { x: 450, y: 0, width: 150, height: 300, carLeftEdge: 450, carRightEdge: 600, carTop: 0, carBottom: 300, color: 'orange', orientation: 'vrt', hasMoved: false, initialPosition: { carLeftEdge: 450, carRightEdge: 600, carTop: 0, carBottom: 300 } },
     { x: 450, y: 450, width: 300, height: 150, carLeftEdge: 450, carRightEdge: 750, carTop: 450, carBottom: 600, color: 'blue', orientation: 'hrz', hasMoved: false, initialPosition: { carLeftEdge: 0, carRightEdge: 300, carTop: 300, carBottom: 450 } },
@@ -28,8 +38,46 @@ const levelData = {
     { x: 600, y: 600, width: 150, height: 300, carLeftEdge: 600, carRightEdge: 750, carTop: 600, carBottom: 900, color: 'brown', orientation: 'vrt', hasMoved: false, initialPosition: { carLeftEdge: 600, carRightEdge: 750, carTop: 600, carBottom: 900 } },
   ],
 
+  4: [
+    { "x": 150, "y": 0, "width": 150, "height": 450, "carLeftEdge": 150, "carRightEdge": 300, "carTop": 0, "carBottom": 450, "color": "#ffff00", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 150, "carRightEdge": 300, "carTop": 0, "carBottom": 450 } },
+    { "x": 300, "y": 0, "width": 300, "height": 150, "carLeftEdge": 300, "carRightEdge": 600, "carTop": 0, "carBottom": 150, "color": "#808000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 600, "carTop": 0, "carBottom": 150 } },
+    { "x": 600, "y": 0, "width": 300, "height": 150, "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150, "color": "#ffa500", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150 } },
+    { "x": 750, "y": 150, "width": 150, "height": 300, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 450, "color": "#87cefa", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 450 } },
+    { "x": 450, "y": 300, "width": 300, "height": 150, "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 750, "carTop": 300, "carBottom": 450 } },
+    { "x": 150, "y": 450, "width": 150, "height": 300, "carLeftEdge": 150, "carRightEdge": 300, "carTop": 450, "carBottom": 750, "color": "#ffc0cb", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 150, "carRightEdge": 300, "carTop": 450, "carBottom": 750 } },
+    { "x": 450, "y": 450, "width": 150, "height": 300, "carLeftEdge": 450, "carRightEdge": 600, "carTop": 450, "carBottom": 750, "color": "#7e00a5", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 600, "carTop": 450, "carBottom": 750 } },
+    { "x": 600, "y": 450, "width": 300, "height": 150, "carLeftEdge": 600, "carRightEdge": 900, "carTop": 450, "carBottom": 600, "color": "#006400", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 900, "carTop": 450, "carBottom": 600 } },
+    { "x": 600, "y": 600, "width": 150, "height": 300, "carLeftEdge": 600, "carRightEdge": 750, "carTop": 600, "carBottom": 900, "color": "#808080", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 750, "carTop": 600, "carBottom": 900 } },
+    { "x": 750, "y": 600, "width": 150, "height": 300, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 600, "carBottom": 900, "color": "#ffc800", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 600, "carBottom": 900 } },
+    { "x": 150, "y": 750, "width": 450, "height": 150, "carLeftEdge": 150, "carRightEdge": 600, "carTop": 750, "carBottom": 900, "color": "#ff00f2", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 150, "carRightEdge": 600, "carTop": 750, "carBottom": 900 } }
+  ],
 
-    
+  5: [
+    { "x": 300, "y": 0, "width": 150, "height": 300, "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 300, "color": "#00c500", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 450, "carTop": 0, "carBottom": 300 } },
+    { "x": 450, "y": 0, "width": 450, "height": 150, "carLeftEdge": 450, "carRightEdge": 900, "carTop": 0, "carBottom": 150, "color": "#ffff00", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 900, "carTop": 0, "carBottom": 150 } },
+    { "x": 750, "y": 150, "width": 150, "height": 300, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 450, "color": "#ffa500", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 150, "carBottom": 450 } },
+    { "x": 0, "y": 300, "width": 300, "height": 150, "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450 } },
+    { "x": 300, "y": 300, "width": 150, "height": 300, "carLeftEdge": 300, "carRightEdge": 450, "carTop": 300, "carBottom": 600, "color": "#87cefa", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 450, "carTop": 300, "carBottom": 600 } },
+    { "x": 0, "y": 450, "width": 300, "height": 150, "carLeftEdge": 0, "carRightEdge": 300, "carTop": 450, "carBottom": 600, "color": "#ffc0cb", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 300, "carTop": 450, "carBottom": 600 } },
+    { "x": 600, "y": 450, "width": 150, "height": 300, "carLeftEdge": 600, "carRightEdge": 750, "carTop": 450, "carBottom": 750, "color": "#7e00a5", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 750, "carTop": 450, "carBottom": 750 } },
+    { "x": 750, "y": 450, "width": 150, "height": 450, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 450, "carBottom": 900, "color": "#ff00f2", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 450, "carBottom": 900 } },
+    { "x": 150, "y": 600, "width": 150, "height": 300, "carLeftEdge": 150, "carRightEdge": 300, "carTop": 600, "carBottom": 900, "color": "#006400", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 150, "carRightEdge": 300, "carTop": 600, "carBottom": 900 } },
+    { "x": 300, "y": 600, "width": 300, "height": 150, "carLeftEdge": 300, "carRightEdge": 600, "carTop": 600, "carBottom": 750, "color": "#808080", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 600, "carTop": 600, "carBottom": 750 } },
+    { "x": 300, "y": 750, "width": 450, "height": 150, "carLeftEdge": 300, "carRightEdge": 750, "carTop": 750, "carBottom": 900, "color": "#0000ff", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 750, "carTop": 750, "carBottom": 900 } }
+  ],
+
+  6: [
+    { "x": 450, "y": 0, "width": 150, "height": 300, "carLeftEdge": 450, "carRightEdge": 600, "carTop": 0, "carBottom": 300, "color": "#00c500", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 600, "carTop": 0, "carBottom": 300 } },
+    { "x": 600, "y": 0, "width": 300, "height": 150, "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150, "color": "#ffa500", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 900, "carTop": 0, "carBottom": 150 } },
+    { "x": 0, "y": 300, "width": 300, "height": 150, "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450, "color": "#ff0000", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 300, "carTop": 300, "carBottom": 450 } },
+    { "x": 300, "y": 300, "width": 150, "height": 300, "carLeftEdge": 300, "carRightEdge": 450, "carTop": 300, "carBottom": 600, "color": "#87cefa", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 300, "carRightEdge": 450, "carTop": 300, "carBottom": 600 } },
+    { "x": 450, "y": 300, "width": 150, "height": 300, "carLeftEdge": 450, "carRightEdge": 600, "carTop": 300, "carBottom": 600, "color": "#ffc0cb", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 450, "carRightEdge": 600, "carTop": 300, "carBottom": 600 } },
+    { "x": 750, "y": 300, "width": 150, "height": 300, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 300, "carBottom": 600, "color": "#7e00a5", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 300, "carBottom": 600 } },
+    { "x": 600, "y": 450, "width": 150, "height": 300, "carLeftEdge": 600, "carRightEdge": 750, "carTop": 450, "carBottom": 750, "color": "#006400", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 600, "carRightEdge": 750, "carTop": 450, "carBottom": 750 } },
+    { "x": 0, "y": 600, "width": 150, "height": 300, "carLeftEdge": 0, "carRightEdge": 150, "carTop": 600, "carBottom": 900, "color": "#808080", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 0, "carRightEdge": 150, "carTop": 600, "carBottom": 900 } },
+    { "x": 150, "y": 600, "width": 450, "height": 150, "carLeftEdge": 150, "carRightEdge": 600, "carTop": 600, "carBottom": 750, "color": "#ffff00", "orientation": "hrz", "hasMoved": false, "initialPosition": { "carLeftEdge": 150, "carRightEdge": 600, "carTop": 600, "carBottom": 750 } },
+    { "x": 750, "y": 600, "width": 150, "height": 300, "carLeftEdge": 750, "carRightEdge": 900, "carTop": 600, "carBottom": 900, "color": "#ffc800", "orientation": "vrt", "hasMoved": false, "initialPosition": { "carLeftEdge": 750, "carRightEdge": 900, "carTop": 600, "carBottom": 900 } }
+  ],
 };
 
 // export default allows data to be shared out of this page (e.g. can now be seen in the mani script.js page)
